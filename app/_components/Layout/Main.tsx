efc9fd4137b7fd8af6b6c357b58a6ef6c9bf7543
cst9221 <chltmdxo3@gmail.com>
2023-11-19 23:51:53 +0900

    MODIFY width max size

--- app/_components/Layout/Main.tsx
+++ app/_components/Layout/Main.tsx
@@ -2,7 +2,7 @@ interface Props extends React.HtmlHTMLAttributes<HTMLElement> {}
 const Main = ({ children, ...props }: Props) => {
   return (
     <main
-      className="grid grid-cols-6 gap-2 mx-8 mt-8"
+      className="grid grid-cols-6 gap-2 w-[900px] min-[900px]:mx-auto mx-8  mt-8"
       {...props}
     >
       {children}
