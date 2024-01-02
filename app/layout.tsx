1ae78ab79e02577369a2349507dcac8cc1846c8a
cst9221 <chltmdxo3@gmail.com>
2024-01-02 22:19:54 +0900

    MODIFY home layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next"
+import type {Metadata} from "next"
 
-import { Inter } from "next/font/google"
+import {Inter} from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: {
     default: "최승태 블로그",
-    template: "최승태 블로그 | %s",
+    template: "최승태 블로그 | %s"
   },
   description: "다양한 삽질들을 담아내기 위한 사이트입니다",
   icons: {
-    icon: "/favicon.ico",
-  },
+    icon: "/favicon.ico"
+  }
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="ko">
       <head></head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
