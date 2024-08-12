c7d93c5b2bfcb066cb3c94985fa7f5220f59a1c8
cst9221 <chltmdxo3@gmail.com>
2024-08-13 00:09:11 +0900

    Add secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,16 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
         }),
     ],
+    session: {
+        strategy: "jwt",
+    },
     debug: process.env.NODE_ENV === "development",
 };
 
