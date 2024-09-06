44b5538ecb958da613a9310893c2fc4893298e52
cst9221 <chltmdxo3@gmail.com>
2024-09-06 13:41:30 +0900

    feat: lighthouse ci 추가

---
+++ .lighthouserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    ci: {
+        collect: {
+            startServerCommand: "npm run start",
+            startServerReadyPattern: "ready on",
+            url: ["http://localhost:3000"],
+            numberOfRuns: 1,
+            // setting: {
+            //     preset: "desktop",
+            // }
+        },
+        assert: {
+            // preset: "lighthouse:recommended",
+        },
+        upload: {
+            // target: "temporary-public-storage"
+        },
+        server: {
+        },
+        wizard: {
+        },
+    },
+};
